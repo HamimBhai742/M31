408c13b500a5ed5178ff026f7a671dc7f437c8f3
HamimBhai742 <omagootruelove147@gmail.com>
2024-02-24 10:17:14 -0800

    first commit

---
+++ map.js
@@ -0,0 +1,37 @@
+const num = [43, 56, 78, 97, 2, 12, 34, 56, 88, 64, 23]
+
+// const newNum = []
+// function numbers(num) {
+//     console.log(num);
+//     for (const nums of num) {
+//         const number = nums * 2
+//         newNum.push(number)
+//         // console.log(newNum);
+
+//     }
+//     return newNum
+// }
+// console.log(numbers(num));
+
+// function num1(number) {
+//     const newNum = number * 3
+//     return newNum
+// }
+
+
+// const number = num => num * num;
+
+
+
+// const result = num.map(number => number + 7)
+// console.log(result);
+
+
+const friends = ['sakiba', 'rakibws', 'hasanssa', 'rokibul', 'tashin', 'ratul', 'komol', 'hossain', 'tuhin', 'khalid']
+
+const fr = fr => {
+    const frLength = fr[3]
+    return frLength
+}
+const result = friends.map(fr)
+console.log(result);
